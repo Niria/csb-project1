1b3897da4fd11fc21ae7d6eae0aba03c03b3e795
Matias Rinne <matias.rinne@helsinki.fi>
2026-05-19 14:11:45 +0300

    add model and edit settings

--- message_board/models.py
+++ message_board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Thread(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.CharField(max_length=4096)
+    upvotes = models.IntegerField(default=0)
+
+class Reply(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    content = models.CharField(max_length=4096)
+    upvotes = models.IntegerField(default=0)
--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "message_board.apps.MessageBoardConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
