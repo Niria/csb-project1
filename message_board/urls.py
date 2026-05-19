c70adb80a2b30515dad33bd5b1e60151c1f218b7
Matias Rinne <matias.rinne@helsinki.fi>
2026-05-19 16:48:51 +0300

    add templates

--- message_board/urls.py
+++ message_board/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = "message_board"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:thread_id>/", views.thread, name="thread")
+    path("threads/<int:thread_id>/", views.thread, name="thread"),
+    path("threads/new/", views.new_thread, name="new_thread"),
+    path("threads/<int:thread_id>/edit/", views.edit_thread, name="edit_thread"),
+    path("threads/<int:thread_id>/reply/", views.new_reply, name="new_reply"),
+    path("threads/<int:thread_id>/<int:reply_id>/edit", views.edit_reply, name="edit_reply"),
 ]
--- message_board/views.py
+++ message_board/views.py
@@ -1,7 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from .models import Thread, Reply
+
 
 def index(request):
-    return HttpResponse("Hello, world")
+    thread_list = Thread.objects.all()
+    context = {"thread_list": thread_list}
+    return render(request, "message_board/index.html", context)
 
 def thread(request, thread_id):
-    return HttpResponse("Thread id: %s" % thread_id)
+    try:
+        thread = Thread.objects.get(pk=thread_id)
+    except:
+        raise Http404("Thread does not exist")
+    replies = Reply.objects.get(thread=thread_id)
+    return render(request, "message_board/thread.html", 
+                  {"thread": thread, "replies": replies})
+
+def new_thread(request):
+    pass
+
+def edit_thread(request, thread_id):
+    pass
+
+def new_reply(request, thread_id):
+    pass
+
+def edit_reply(request, thread_id, reply_id):
+    pass
