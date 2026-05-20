0ab34d2915e4360c29fe19e0c8e3ee8b97012567
Matias Rinne <matias.rinne@helsinki.fi>
2026-05-20 15:26:16 +0300

    add views for login and logout, add template for thread content and replies

--- message_board/urls.py
+++ message_board/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "message_board"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("login", views.login_view, name="login_view"),
+    path("logout", views.logout_view, name="logout_view"),
     path("threads/<int:thread_id>/", views.thread, name="thread"),
     path("threads/new/", views.new_thread, name="new_thread"),
-    path("threads/<int:thread_id>/edit/", views.edit_thread, name="edit_thread"),
+    path("threads/<int:thread_id>/delete/", views.delete_thread, name="delete_thread"),
     path("threads/<int:thread_id>/reply/", views.new_reply, name="new_reply"),
-    path("threads/<int:thread_id>/<int:reply_id>/edit", views.edit_reply, name="edit_reply"),
+    path("threads/<int:thread_id>/<int:reply_id>/delete", views.delete_reply, name="delete_reply"),
 ]
--- message_board/views.py
+++ message_board/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
 from .models import Thread, Reply
 
 
@@ -8,23 +11,50 @@ def index(request):
     context = {"thread_list": thread_list}
     return render(request, "message_board/index.html", context)
 
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "message_board/login.html")
+    elif request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("message_board:index")
+        else:
+            return redirect("message_board:login_view", {"error": "Invalid username or password"})
+        
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+    return redirect("message_board:index")
+
 def thread(request, thread_id):
-    try:
-        thread = Thread.objects.get(pk=thread_id)
-    except:
-        raise Http404("Thread does not exist")
-    replies = Reply.objects.get(thread=thread_id)
+    thread = get_object_or_404(Thread, pk=thread_id)
+    replies = Reply.objects.all().filter(thread=thread_id)
     return render(request, "message_board/thread.html", 
                   {"thread": thread, "replies": replies})
 
 def new_thread(request):
-    pass
+    thread_title = request.POST["new-thread-title"]
+    thread_content = request.POST["new-thread-content"]
+    thread = Thread(title=thread_title, content=thread_content)
+    thread.save()
+    return redirect("message_board:thread", thread_id=thread.id)
 
-def edit_thread(request, thread_id):
-    pass
+def delete_thread(request, thread_id):
+    thread = get_object_or_404(Thread, pk=thread_id)
+    thread.delete()
+    return redirect("message_board:index")
 
 def new_reply(request, thread_id):
-    pass
+    thread = get_object_or_404(Thread, pk=thread_id)
+    reply_content = request.POST["new-reply-content"]
+    reply = Reply(thread=thread, content=reply_content)
+    reply.save()
+    return HttpResponseRedirect(reverse("message_board:thread", args=(thread_id,)))
 
-def edit_reply(request, thread_id, reply_id):
-    pass
+def delete_reply(request, thread_id, reply_id):
+    reply = get_object_or_404(Reply, pk=reply_id)
+    reply.delete()
+    return redirect("message_board:thread", thread_id=thread_id)
